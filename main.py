9273528e9790e004b84a5fdba2686fd54e8f71f1
João Pedro Braz <joao.braz@ekaizen-br.com>
2020-04-01 13:57:23 -0300

    Refatorado para classe Main, com um driver code

--- main.py
+++ main.py
@@ -2,61 +2,64 @@ from user import User
 from server import Server
 
 
-def create_new_server_for_user(servers):
-    new_server = Server(total_of_users_per_server)
-    servers.append(new_server)
-    new_server.append_user(User(total_ticks))
+class Main():
 
+    def __init__(self, total_ticks, total_of_users_per_server, users_per_tick, servers=None):
+        self.total_ticks = total_ticks
+        self.total_of_users_per_server = total_of_users_per_server
+        self.users_per_tick = users_per_tick
+        self.servers = servers if servers is not None else [
+            Server(self.total_of_users_per_server)]
+        self.total_cost = 0
 
-def searched_the_last(servers, server):
-    return servers[len(servers) - 1] == server
+    def create_new_server_for_user(self):
+        new_server = Server(self.total_of_users_per_server)
+        self.servers.append(new_server)
+        return new_server
 
+    def is_the_last_server(self, server):
+        return self.servers[len(self.servers) - 1] == server
 
-def get_the_last_server(servers):
-    if len(servers) == 0:
-        return None
-    return servers[len(servers) - 1]
+    def get_the_last_server(self):
+        if len(self.servers) == 0:
+            return None
+        return self.servers[len(self.servers) - 1]
+
+    def handle_new_user(self):
+        last_server = self.get_the_last_server()
+        if last_server is None or last_server.is_full():
+            last_server = self.create_new_server_for_user()
+        last_server.append_user(User(self.total_ticks))
+
+    def handle_server(self, server):
+        # tick, and remove if necessary, all the server's users
+        server.tick_users()
+        if server.is_empty():
+            # before we remove this server, calculate it's cost
+            self.total_cost += server.ticks * Server.COST_PER_TICK
+            self.servers.remove(server)
+        else:
+            server.tick()
 
 
 if __name__ == "__main__":
-    total_ticks = 4
-    total_of_users_per_server = 2
-    users_per_tick = [1, 3, 0, 1, 0, 1]
-    servers = [Server(total_of_users_per_server)]
-    total_cost = 0
+    main = Main(4, 2, [1, 3, 0, 1, 0, 1], [Server(2)])
     tick = 0
 
-    while len(servers) > 0 or tick < len(users_per_tick):
-        if tick < len(users_per_tick):
-            for _ in range(users_per_tick[tick]):
-                last_server = get_the_last_server(servers)
-                if last_server is None:
-                    last_server = Server(total_of_users_per_server)
-                    servers.append(last_server)
-                if last_server.is_full():
-                    if searched_the_last(servers, last_server):
-                        # If we've searched all servers
-                        create_new_server_for_user(servers)
-                else:
-                    # Avaliable server found, append user and stop searching
-                    last_server.append_user(User(total_ticks))
+    while len(main.servers) > 0 or tick < len(main.users_per_tick):
+        if tick < len(main.users_per_tick):
+            for _ in range(main.users_per_tick[tick]):
+                main.handle_new_user()
         total_users = []
         # To avoid changing the list in place
-        _servers = servers.copy()
+        _servers = main.servers.copy()
         for server in _servers:
-            # tick, and remove if necessary, all the server's users
-            server.tick_users()
-            if server.is_empty():
-                # before we remove this server, calculate it's cost
-                total_cost += server.ticks * Server.COST_PER_TICK
-                servers.remove(server)
-            else:
-                server.tick()
+            main.handle_server(server)
             # Edge case for no more servers are available
-            if len(server.users) > 0 or len(servers) == 0:
+            if len(server.users) > 0 or len(main.servers) == 0:
                 total_users.append(str(len(server.users)))
 
         print('tick:' + str(tick) + ' ' + ','.join(total_users))
         tick += 1
 
-    print(total_cost)
+    print(main.total_cost)
